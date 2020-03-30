4625964aaa0bdc60e73672a10a678457e468cdde
Rafael Pradella <pradella@outlook.com>
2020-03-30 15:39:10 -0300

    Add .gitignore to yarn

--- src/.temp/routes.js
+++ src/.temp/routes.js
@@ -1,22 +1,22 @@
 export default [
   {
     path: "/about/",
-    component: () => import(/* webpackChunkName: "page--src--pages--about-vue" */ "/Users/rafaelpradella/Dev/map/src/pages/About.vue")
+    component: () => import(/* webpackChunkName: "page--src-pages-about-vue" */ "C:\\Users\\prade\\pradelladev\\src\\pages\\About.vue")
   },
   {
     name: "404",
     path: "/404/",
-    component: () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/Users/rafaelpradella/Dev/map/node_modules/gridsome/app/pages/404.vue")
+    component: () => import(/* webpackChunkName: "page--node-modules-gridsome-app-pages-404-vue" */ "C:\\Users\\prade\\pradelladev\\node_modules\\gridsome\\app\\pages\\404.vue")
   },
   {
     name: "home",
     path: "/",
-    component: () => import(/* webpackChunkName: "page--src--pages--index-vue" */ "/Users/rafaelpradella/Dev/map/src/pages/Index.vue")
+    component: () => import(/* webpackChunkName: "page--src-pages-index-vue" */ "C:\\Users\\prade\\pradelladev\\src\\pages\\Index.vue")
   },
   {
     name: "*",
     path: "*",
-    component: () => import(/* webpackChunkName: "page--node-modules--gridsome--app--pages--404-vue" */ "/Users/rafaelpradella/Dev/map/node_modules/gridsome/app/pages/404.vue")
+    component: () => import(/* webpackChunkName: "page--node-modules-gridsome-app-pages-404-vue" */ "C:\\Users\\prade\\pradelladev\\node_modules\\gridsome\\app\\pages\\404.vue")
   }
 ]
 
