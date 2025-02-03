de2338e974284e959a08fd30520d9899557c7ae6
Rafael Pradella <pradella@outlook.com>
2025-02-03 01:44:20 +0000

    [wip] Still choppy but barely functional offset-based animation

--- src/components/MapAnimation/LineAnimator.vue
+++ src/components/MapAnimation/LineAnimator.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
-import { flow, pipe } from "effect";
 import { onMounted, useTemplateRef } from "vue";
+import { pipe } from "effect";
 import { fromNullable, map, match } from "effect/Option";
 
 import { logPipe } from "@utils/debugUtils.ts";
@@ -47,7 +47,6 @@ onMounted(() => {
         map(splitPathBySegments),
         logPipe('split'),
         map(createKeyframeFromCoordinatesList),
-        logPipe('keyframe'),
         match({ onNone: console.error, onSome: createAnimation })
     );
 });
@@ -61,23 +60,10 @@ onMounted(() => {
     </g>
 </template>
 <style>
-/* @keyframes paths {
-    0% { offset: path("M286.057 313.635L321.261 370.664") 0%; }
-    25% { offset: path("M286.057 313.635L321.261 370.664") 100%;}
-    25.01% { offset: path("M321.261 370.664H355.056") 0%; }
-    50% { offset: path("M321.261 370.664H355.056") 100%;}
-    50.01% { offset: path("M355.056 370.664H428.279") 0%; }
-    75% { offset: path("M355.056 370.664H428.279") 100%;}
-    75.01% { offset: path("M428.279 370.664H481.084") 0%; }
-    100% { offset: path("M428.279 370.664H481.084") 100%;}
-} */
 
 .lineAnimator {
     circle {
         fill: var(--background);
-        /* animation: paths 30s cubic-bezier(.3,0,.3,1) forwards infinite;
-        fill: var(--background); */
-        opacity: 1;
     }
 
     .u-lineStroke {
@@ -85,46 +71,3 @@ onMounted(() => {
     }
 }
 </style>
-<!-- 
-<script>
-// Web Animation API Keyframes version
-const paths = [
-    "M286.057 313.635L321.261 370.664",
-    "M321.261 370.664H355.056",
-    "M355.056 370.664H428.279",
-    "M428.279 370.664H481.084"
-];
-
-const keyframes = paths.flatMap((path, index) => {
-    const segmentSize = 1 / paths.length;
-    const startOffset = index * segmentSize;
-    const endOffset = (index + 1) * segmentSize;
-    
-    return [
-        {
-            offset: startOffset,
-            offsetPath: `path("${path}")`,
-            offsetDistance: "0%"
-        },
-        {
-            offset: endOffset,
-            offsetPath: `path("${path}")`,
-            offsetDistance: "100%"
-        }
-    ];
-});
-</script>
-<style>
-.lineAnimator {
-    circle {
-        fill: var(--background);
-        animation: paths 30s cubic-bezier(.3,0,.3,1) forwards infinite;
-        fill: var(--background);
-        opacity: 1;
-    }
-
-    .u-lineStroke {
-        fill: v-bind(lineColorVariable);
-    }
-}
-</style> -->
--- src/utils/animationUtils.ts
+++ src/utils/animationUtils.ts
@@ -3,20 +3,11 @@ const ANIMATION_EASE = "cubic-bezier(.3,0,.3,1)";
 const createKeyframeFromCoordinatesList = (svgPathList: string[]) => {
   const isLastIndex = (i: number) => i === svgPathList?.length - 1;
 
-  return svgPathList.flatMap((path, i) => [
-    {
+  return svgPathList.map((path, i) => ({
       easing: ANIMATION_EASE,
       offsetDistance: '0%',
       offsetPath: `path("${path}")`,
-      opacity: i === 0 ? 0 : 1,
-    },
-    {
-      easing: ANIMATION_EASE,
-      offsetDistance: '100%',
-      offsetPath: `path("${path}")`,
-      opacity: isLastIndex(i) ? 0 : 1,
-    }
-  ]) as Keyframe[];
-};
+    })
+)};
 
 export { createKeyframeFromCoordinatesList };
